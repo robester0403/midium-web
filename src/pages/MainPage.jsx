c09073ac484ea54b92455d1d7038afdf2f3127c9
robester0403 <robkso@gmail.com>
2022-12-08 09:20:44 -0500

    refactored styling

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar/NavBar";
-import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
+import {
+  LayoutContainer,
+  TempPhoneContainer,
+  ViewContainer,
+} from "./MainPageStyle";
 
 const MainPage = () => {
   return (
@@ -21,21 +25,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-const TempPhoneContainer = styled.div`
-  max-width: 480px;
-
-  margin: 0 auto;
-`;
-
-const LayoutContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-sizing: border-box;
-`;
-
-const ViewContainer = styled.div`
-  padding: 0 24px;
-  box-sizing: border-box;
-`;
